6487f029a97a4d55d4d200fc02d1b7f7ee795b61
Rownak <muntasir@hrms.local>
2026-07-17 00:13:31 +0600

    Fix Vercel PHP boot: correct dont-discover (nunomaduro/*), regenerate clean packages.php manifest

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -74,20 +74,6 @@
       0 => 'Carbon\\Laravel\\ServiceProvider',
     ),
   ),
-  'nunomaduro/collision' => 
-  array (
-    'providers' => 
-    array (
-      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
-    ),
-  ),
-  'nunomaduro/termwind' => 
-  array (
-    'providers' => 
-    array (
-      0 => 'Termwind\\Laravel\\TermwindServiceProvider',
-    ),
-  ),
   'pestphp/pest-plugin-laravel' => 
   array (
     'providers' => 
